175f80802af55df7b7961bf520a49433b7845eb7
gambalongadev <gambalongadev@gmail.com>
2024-03-10 22:54:37 +0100

    Refactor layout and add Navbar component

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,8 +1,13 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import {Navbar} from "@/components"
+
+export default function GeneralLayout({ children }: { children: React.ReactNode }) {
   return (
+    <>
+    <Navbar />
     <main className="flex flex-col items-center p-24">
-      <span className="text-lg">About Page</span>
+      <span className="text-lg">Hola Mundo</span>
       {children}
     </main>
+    </>
   );
 }
